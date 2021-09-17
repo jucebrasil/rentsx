906bf794e695b9009e442b8dfabf65f628fd7ac7
Juce Brasil <jucebrasil.ti@gmail.com>
2021-09-17 00:14:10 -0400

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Container,
+  Title,
+} from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Title>
+        HOME
+      </Title>
+
+    </Container>
+  );
+}
