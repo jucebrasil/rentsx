bbb36079328eb5184d01791b646bddc7f36e2535
Juce Brasil <jucebrasil.ti@gmail.com>
2021-09-18 22:08:16 -0400

    Continuando a interface de Detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -5,7 +5,16 @@ import { ImageSlider } from '../../components/ImageSlider';
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
 } from './styles';
 
 export function CarDetails() {
@@ -18,6 +27,28 @@ export function CarDetails() {
       <CarImages>
         <ImageSlider imageUrl={['https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png']} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+
+        </Details>
+
+        <About>
+          Este é automóvel desportivo.
+          Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
+      </Content>
+
     </Container>
   );
 }
