bd372250b0bc47183a405b4613ac953331fffddc
Juce Brasil <jucebrasil.ti@gmail.com>
2021-09-18 21:21:32 -0400

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
   Header,
+  CarImages
 } from './styles';
 
 export function CarDetails() {
@@ -13,6 +15,9 @@ export function CarDetails() {
         <BackButton onPress={() => { }} />
       </Header>
 
+      <CarImages>
+        <ImageSlider imageUrl={['https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png']} />
+      </CarImages>
     </Container>
   );
 }
