2b79e422e2047c7d001592911cb29cd03eef24b0
Juce Brasil <jucebrasil.ti@gmail.com>
2021-09-18 23:33:25 -0400

    Componente de Acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Accessory } from '../../components/Accessory/Index';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -14,7 +22,8 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Acessories
 } from './styles';
 
 export function CarDetails() {
@@ -40,6 +49,15 @@ export function CarDetails() {
           </Rent>
 
         </Details>
+
+        <Acessories>
+          <Accessory name={'380Km/h'} icon={speedSvg} />
+          <Accessory name={'3.2s'} icon={accelerationSvg} />
+          <Accessory name={'800 Hp'} icon={forceSvg} />
+          <Accessory name={'Gadolina'} icon={gasolineSvg} />
+          <Accessory name={'Auto'} icon={exchangeSvg} />
+          <Accessory name={'2 pessoas'} icon={peopleSvg} />
+        </Acessories>
 
         <About>
           Este é automóvel desportivo.
